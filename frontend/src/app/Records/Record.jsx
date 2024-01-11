2ce2f5ad6e2328e61064b51d926252991c046bce
Munkherdene <b.munkherdene28@gmail.com>
2024-01-11 20:37:54 +0800

    add Record in frontend

--- frontend/src/app/Records/Record.jsx
+++ frontend/src/app/Records/Record.jsx
@@ -4,6 +4,7 @@ import Plus from "../SVG/Plus";
 import Eye from "../SVG/Eye";
 import Icon from "../SVG/Icon";
 import Blue from "../SVG/Blue";
+import Delete from "../SVG/Delete";
 import Select from "../Records/Select";
 import React, { useState } from "react";
 export default function record() {
@@ -148,6 +149,88 @@ export default function record() {
         </div>
         <Select />
       </div>
+      {isShow && (
+        <div
+          className={`flex justify-center items-center bg-gray-100 opacity-1 h-screen `}
+        >
+          <div className="flex w-[792px] h-[510px] flex-col items-start bg-base-100 p-4 rounded-lg bg-white gap-3">
+            <div className="flex justify-between w-[750px] items-center">
+              <h1 className="text-xl font-semibold">Add Record</h1>
+              <button onClick={handleAddButtonClick}>
+                <Delete />
+              </button>
+            </div>
+            <hr />
+            <div className="flex gap-20 justify-center">
+              <div className="flex flex-col gap-6">
+                <div>
+                  <div className="join">
+                    <input
+                      className="join-item btn w-[160px]"
+                      type="radio"
+                      name="options"
+                      aria-label="Expense"
+                    />
+                    <input
+                      className="join-item btn w-[160px]"
+                      type="radio"
+                      name="options"
+                      aria-label="Income"
+                    />
+                  </div>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Amount ₮ 000,00"
+                  className="input input-bordered input-lg flex flex-col"
+                />
+                <div className="flex flex-col gap-2">
+                  <h1>Category</h1>
+                  <select className="select select-bordered bg-gray-50">
+                    <option>Find or choose category</option>
+                    <option>Home</option>
+                    <option>Gift</option>
+                  </select>
+                </div>
+                <div className="flex gap-2">
+                  <div className="flex flex-col gap-2 w-full">
+                    <h1>Date</h1>
+                    <select className="select select-bordered bg-gray-50">
+                      <option>Oct 30, 2023</option>
+                    </select>
+                  </div>
+                  <div className="flex flex-col gap-2 w-full">
+                    <h1>Date</h1>
+                    <select className="select select-bordered bg-gray-50">
+                      <option>4:15 PM</option>
+                    </select>
+                  </div>
+                </div>
+                <button className="w-full h-[45px] bg-blue-600 rounded-3xl text-white">
+                  Add Record
+                </button>
+              </div>
+              <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-2">
+                  <h1>Payee</h1>
+                  <select className="select select-bordered bg-gray-50 w-[350px]">
+                    <option>Write here</option>
+                    <option>Han Solo</option>
+                    <option>Greedo</option>
+                  </select>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <h1>Note</h1>
+                  <textarea
+                    className="textarea textarea-bordered w-[350px] h-[293px]"
+                    placeholder="Write here"
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
